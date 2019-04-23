47baf3f30cb474e32b739345b6b1226022648720
Scott <scottgangemi@gmail.com>
2019-04-23 21:50:10 +1000

    Fix deploy?

--- config/prod.exs
+++ config/prod.exs
@@ -10,4 +10,8 @@ config :mirror, MirrorWeb.Endpoint,
 config :logger, level: :info
 
 # Configure your database
-import_config "prod.secret.exs"
+config :mirror, Mirror.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
